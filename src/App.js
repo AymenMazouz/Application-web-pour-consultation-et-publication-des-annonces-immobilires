1ab56322a2c69707d27598817624d943cef08576
lyeschalal <kl_chalal@esi.dz>
2023-01-21 00:42:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom"
+import Home from './Pages/index';
+import  SigninPage  from "./Pages/SignIn/Signin"
+import SigninPageUser from "./Pages/SignIn/SignInUser/SigninPageUser"
+import SigninPagepass from "./Pages/SignIn/SignInAdmin/SigninPagepass"
+import MainPage from "./Pages/Main"
+import SignInAdminPage from './Pages/SignIn/SignInAdmin/SignInAdmin';
+import AdminPage from './Pages/SignIn/SignInAdmin/Admin';
+import Page from './Pages/Rechercheetfiltre/Lyes';
+import AnnoncePage from './Pages/MyAI/annonce';
+import DetailAI from './Pages/DetailAI/detailAI';
+import Deposerai from './Pages/DeposerAI/deposerAI';
 
-function App() {
+
+
+
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Signin" element={<SigninPage/>} />
+      <Route path="/Signin/Signinuser" element={<SigninPageUser />} />
+      <Route path="/Signin/SigninAdminPass" element={<SigninPagepass />} />
+      <Route path="/Signin/SigninUser/Main" element={<MainPage />} />
+      <Route path="/Signin/SigninAdminPass/SigninAdmin" element={<SignInAdminPage />} />
+      <Route path="/Signin/SigninAdminPass/SigninAdmin/Admin" element={<AdminPage />} />
+      <Route path="/Signin/SigninUser/Main/lyes" element={<Page />} />
+      <Route path="/Signin/SigninUser/Main/annonce" element={<AnnoncePage />} /> 
+      <Route path="/Signin/SigninUser/Main/lyes/detaiAI/:id" element={<DetailAI />} />
+      <Route path="/Signin/SigninUser/Main/deposerai" element={<Deposerai />} />
+
+      
+
+
+
+    </Routes>
   );
 }
 
